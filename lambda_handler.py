8dcd959005d2251e104e1a6c4fe0ca129a004930
Kishan Silawat <kishan.silawat@skyflow.com>
2022-11-10 20:07:12 +0530

    Added failure case

--- lambda_handler.py
+++ lambda_handler.py
@@ -50,43 +50,60 @@ def lambda_handler(event, context):
             f"{job_id} Creating Source Location with source_bucket_arn:"
             f" {source_bucket_arn}, source_subdirectory: {source_subdirectory}"
         )
-        source_location_arn = client.create_location_s3(
-            S3BucketArn=source_bucket_arn,
-            Subdirectory=source_subdirectory,
-            S3StorageClass="STANDARD",
-            S3Config={
-                "BucketAccessRoleArn": bucket_access_role_arn,
-            },
-        ).get("LocationArn")
-        print(f"{job_id}-> Created Source Location ARN")
+        try:
+            source_location_arn = client.create_location_s3(
+                S3BucketArn=source_bucket_arn,
+                Subdirectory=source_subdirectory,
+                S3StorageClass="STANDARD",
+                S3Config={
+                    "BucketAccessRoleArn": bucket_access_role_arn,
+                },
+            ).get("LocationArn")
+            print(f"{job_id}-> Created Source Location ARN")
 
-        destination_bucket_arn = f"arn:aws:s3:::{destination_bucket_name}"
-        destination_subdirectory = destination_path
-        print(
-            f"{job_id} Creating Destination Location with "
-            f"destination_bucket_arn: {destination_bucket_arn}, "
-            f"destination_subdirectory: {destination_subdirectory}"
-        )
-        destination_location_arn = client.create_location_s3(
-            S3BucketArn=destination_bucket_arn,
-            Subdirectory=destination_subdirectory,
-            S3StorageClass="STANDARD",
-            S3Config={
-                "BucketAccessRoleArn": bucket_access_role_arn,
-            },
-        ).get("LocationArn")
-        print(f"{job_id}-> Created Destination Location ARN")
+            destination_bucket_arn = f"arn:aws:s3:::{destination_bucket_name}"
+            destination_subdirectory = destination_path
+            print(
+                f"{job_id} Creating Destination Location with "
+                f"destination_bucket_arn: {destination_bucket_arn}, "
+                f"destination_subdirectory: {destination_subdirectory}"
+            )
+            destination_location_arn = client.create_location_s3(
+                S3BucketArn=destination_bucket_arn,
+                Subdirectory=destination_subdirectory,
+                S3StorageClass="STANDARD",
+                S3Config={
+                    "BucketAccessRoleArn": bucket_access_role_arn,
+                },
+            ).get("LocationArn")
+            print(f"{job_id}-> Created Destination Location ARN")
 
-        task_arn = client.create_task(
-            SourceLocationArn=source_location_arn,
-            DestinationLocationArn=destination_location_arn,
-        ).get("TaskArn")
-        print(f"{job_id}-> Created Task ARN")
+            task_arn = client.create_task(
+                SourceLocationArn=source_location_arn,
+                DestinationLocationArn=destination_location_arn,
+            ).get("TaskArn")
+            print(f"{job_id}-> Created Task ARN")
 
-        task_execution_arn = client.start_task_execution(
-            TaskArn=task_arn,
-        ).get("TaskExecutionArn")
-        print(f"{job_id}-> Created Task Execution ARN")
+            task_execution_arn = client.start_task_execution(
+                TaskArn=task_arn,
+            ).get("TaskExecutionArn")
+            print(f"{job_id}-> Created Task Execution ARN")
+        except Exception as e:
+            print(
+                f"{job_id}-> Updating jobstatus: FAILED, failed to initiate "
+                f"data transfer"
+            )
+            failure_update_query = "UPDATE emr_job_details SET jobstatus=%s " \
+                                   "WHERE id=%s"
+
+            cursor.execute(
+                failure_update_query,
+                ("FAILED", job_id)
+            )
+            connection.commit()
+            cursor.close()
+            print(f"{job_id}-> Successfully updated jobstatus: FAILED")
+            return
 
         update_query = "UPDATE emr_job_details SET task_execution_arn=%s, " \
                        "data_transfer_state=%s WHERE id=%s"
